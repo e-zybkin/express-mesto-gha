5291ecf91dda6f34733227b3360c28b7bf6eb3fb
e-zybkin <zybkin.evgeniy@gmail.com>
2022-03-20 00:37:09 +0300

    create app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
